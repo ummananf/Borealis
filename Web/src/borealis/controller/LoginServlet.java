aa10c80aa8745a4a5810a2c0e8ba32fb3e96edff
Mike Ryczkowski <mryczkowski@gmail.com>
2015-02-03 19:44:18 -0600

    testing login servlet

--- Web/src/borealis/controller/LoginServlet.java
+++ Web/src/borealis/controller/LoginServlet.java
@@ -25,6 +25,7 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		response.getWriter().write("hello");
 	}
 
 	/**
