20dcf485494dc61a99aa0780099fe068cde7c946
mark <markgrush303@gmail.com>
2015-02-12 20:08:23 -0600

    deleted Profile_PE class

--- Web/src/ca/servlets/DBInitServlet.java
+++ Web/src/ca/servlets/DBInitServlet.java
@@ -57,43 +57,7 @@ public class DBInitServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) 
 			throws ServletException, IOException {
 		
-		String message = "";
-		ResultSet resultSet;
-		Statement readStatement;
-		Connection connection = ConnectionManager.getConnection();
-		
-		if (connection != null) {
-			try {
-				Statement setupStatement = connection.createStatement();
-				readStatement = connection.createStatement();
-				resultSet = readStatement.executeQuery("SELECT * FROM UserTable;");
-				while (resultSet.next()) {
-					message += resultSet.getInt(1) + ", ";
-					message += resultSet.getString(2) + ", ";
-					message += resultSet.getString(3) + ", ";
-					message += resultSet.getString(4) + "\n";
-				}
-			    resultSet.close();
-			    readStatement.close();
-			} catch (SQLException ex) {
-			    // handle any errors
-			    System.out.println("SQLException: " + ex.getMessage());
-			    System.out.println("SQLState: " + ex.getSQLState());
-			    System.out.println("VendorError: " + ex.getErrorCode());
-			} finally {
-			    System.out.println("Closing the connection.");
-			    if (connection != null) ConnectionManager.closeConnection(connection);
-			}
-		}
-  	     
-	      // Set response content type
-	      res.setContentType("text/html");
-
-	      // Actual logic goes here.
-	      PrintWriter out = res.getWriter();
-	      out.println("<h1>" + message + "</h1>");
-		
-		// res.sendRedirect("/login");
+		res.sendRedirect("/login");
 
 	}
 
