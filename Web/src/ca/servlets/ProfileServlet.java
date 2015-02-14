4aff93f7ed34873eb91c297cf1447f5f85d965c9
jxmai <maij@myumanitoba.ca>
2015-02-13 20:57:05 -0600

    resolved conflict

--- Web/src/ca/servlets/ProfileServlet.java
+++ Web/src/ca/servlets/ProfileServlet.java
@@ -18,10 +18,6 @@ public class ProfileServlet extends HttpServlet {
 	         throws ServletException, IOException {
 
 		try {
-<<<<<<< HEAD
-			
-=======
->>>>>>> origin/master
 		    User user = new User( req.getParameter( "username" ), req.getParameter( "password" ));
 		    Profile_BO aBO = new Profile_BO(user);
 			System.out.println("u1=" + user.getUsername() + "___ " + user.getPassword());
