9a2b52b3f302cd28cc1709b21cb3be69f1015477
mark <markgrush303@gmail.com>
2015-02-25 18:10:32 -0600

    refactored connectionmanager. added tests.

---
+++ Web/tests/ca/persistence/ConnectionManagerTest.java
@@ -0,0 +1,37 @@
+package ca.persistence;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ConnectionManagerTest {
+	
+	Connection connection;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testGetConnection() {
+		connection = ConnectionManager.getConnection();
+		assertNotNull(connection);
+	}
+
+	@Test
+	public final void testCloseConnection() {
+		ConnectionManager.closeConnection(connection);
+		assertNull(connection);
+	}
+
+}
