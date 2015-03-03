75e754f781200c8387264f83606a75ca806db2d9
mark <markgrush303@gmail.com>
2015-03-02 20:45:21 -0600

    added more model tests

--- Web/tests/ca/persistence/CourseModelTest.java
+++ Web/tests/ca/persistence/CourseModelTest.java
@@ -2,8 +2,12 @@ package ca.persistence;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
+
+import ca.objects.Prereq;
 
 public class CourseModelTest {
 
@@ -13,7 +17,11 @@ public class CourseModelTest {
 
 	@Test
 	public final void testGetPrereqs() {
-		fail("Not yet implemented"); // TODO
+		
+		ArrayList<Prereq> prereqs = CourseModel.getPrereqs("COMP1010");
+		assert(prereqs.size() == 1);
+		Prereq prereq = new Prereq("COMP1010", "COMP1020", 2.0f);
+		assert(prereqs.get(0).equals(prereq));
 	}
 
 }
--- Web/tests/ca/persistence/StudentModelTest.java
+++ Web/tests/ca/persistence/StudentModelTest.java
@@ -2,18 +2,50 @@ package ca.persistence;
 
 import static org.junit.Assert.*;
 
+import java.sql.Date;
+import java.util.ArrayList;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import ca.objects.Enrollment;
+
 public class StudentModelTest {
+	
+	String queryInsertStudent;
+	String queryDeleteStudent;
+	String queryInsertEnrollment;
+	String queryDeleteEnrollment;
 
 	@Before
 	public void setUp() throws Exception {
+		
+		queryInsertStudent = "INSERT INTO Users VALUES (99, 'somestudent', 'p', 'qwe@qwe.qwe', 'john', 'johnson', 'student');";
+		queryDeleteStudent = "DELETE FROM Users WHERE userID=99;";
+		
+		queryInsertEnrollment = "INSERT INTO Enrolled VALUES (99, 'A01', 'COMP1010', '2014-09-01', 3.5);";
+		queryDeleteEnrollment = "DELETE FROM Enrolled WHERE userID=99;";
+		
+		DB.execute(queryDeleteStudent);
+		DB.execute(queryDeleteEnrollment);
+		
+		DB.execute(queryInsertStudent);
+		DB.execute(queryInsertEnrollment);
 	}
 
 	@Test
 	public final void testGetCompletedCourses() {
-		fail("Not yet implemented"); // TODO
+		ArrayList<Enrollment> enrollment = StudentModel.getCompletedCourses(99);
+		assert(enrollment.size() == 1);
+		Enrollment en = new Enrollment(99, "A01", "COMP1010", Date.valueOf("2014-09-01"), 3.5f);
+		assert(enrollment.get(0).equals(en));
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		DB.execute(queryDeleteStudent);
+		DB.execute(queryDeleteEnrollment);
 	}
 
 }
